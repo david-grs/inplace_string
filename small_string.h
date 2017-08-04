bfb19544c1830aa3e040bb19c8bcd40d117f38cb
David G <david.a.grs@gmail.com>
2017-08-04 19:57:25 +0200

    using type aliases

--- small_string.h
+++ small_string.h
@@ -31,7 +31,7 @@ struct basic_small_string
 		_size(0)
 	{}
 
-	basic_small_string(std::size_t count, char ch)
+	basic_small_string(std::size_t count, value_type ch)
 	{
 		if (count > N)
 			throw std::out_of_range("basic_small_string::basic_small_string");
@@ -53,12 +53,12 @@ struct basic_small_string
 		init(other.data() + pos, count);
 	}
 
-	basic_small_string(const char* str, std::size_t count)
+	basic_small_string(const value_type* str, std::size_t count)
 	{
 		init(str, count);
 	}
 
-	basic_small_string(const char* str)
+	basic_small_string(const value_type* str)
 	{
 		init(str, std::strlen(str));
 	}
@@ -69,33 +69,33 @@ struct basic_small_string
 		init(first, last);
 	}
 
-	char& at(std::size_t i)       { return _data.at(i); }
-	char  at(std::size_t i) const { return _data.at(i); }
+	reference at(std::size_t i)         { return _data.at(i); }
+	value_type  at(std::size_t i) const { return _data.at(i); }
 
-	char& operator[](std::size_t i)       { return assert(i < _size); _data[i]; }
-	char  operator[](std::size_t i) const { return assert(i < _size); _data[i]; }
+	reference operator[](std::size_t i)         { return assert(i < _size); _data[i]; }
+	value_type  operator[](std::size_t i) const { return assert(i < _size); _data[i]; }
 
-	char  front() const { return assert(!empty()); _data[0]; }
-	char& front()       { return assert(!empty()); _data[0]; }
-	char  back() const  { return assert(!empty()); _data[_size]; }
-	char& back()        { return assert(!empty()); _data[_size]; }
+	value_type front() const { return assert(!empty()); _data[0]; }
+	reference  front()       { return assert(!empty()); _data[0]; }
+	value_type back() const  { return assert(!empty()); _data[_size]; }
+	reference  back()        { return assert(!empty()); _data[_size]; }
 
-	const char* data() const { return _data.data(); }
-	char*       data()       { return _data.data(); }
+	const value_type* data() const { return _data.data(); }
+	char* data() { return _data.data(); }
 
-	const char* c_str() const { return _data.data(); }
+	const value_type* c_str() const { return _data.data(); }
 
 	operator std::experimental::string_view() const noexcept { return {_data.data(), _size}; }
 
-	bool empty() const { return _size == 0; }
-	std::size_t size() const   { return _size; }
-	std::size_t length() const { return _size; }
+	bool empty() const           { return size() == 0; }
+	std::size_t length() const   { return size(); }
+	std::size_t size() const     { return _size; }
 	std::size_t max_size() const { return N; }
 	std::size_t capacity() const { return N; }
 	void shrink_to_fit() {}
 
 private:
-	void init(const char* str, std::size_t count)
+	void init(const value_type* str, std::size_t count)
 	{
 		if (count > N)
 			throw std::out_of_range("basic_small_string::init");
@@ -115,6 +115,7 @@ private:
 		_size = count;
 	}
 
+	// TODO compressed pair, alignement
 	std::array<char, N> _data;
 	char _size;
 };
