b3ad0a995255de14075f5b908f166db22bfc2bd6
David G <david.a.grs@gmail.com>
2017-08-04 15:05:02 +0200

    adding ctors

--- small_string.h
+++ small_string.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <array>
+#include <cassert>
+#include <algorithm>
+#include <cstring>
 
 namespace cxx { namespace detail
 {
@@ -8,10 +11,72 @@ namespace cxx { namespace detail
 template <std::size_t N>
 struct basic_small_string
 {
+	basic_small_string() :
+		_size(0)
+	{}
+
+	basic_small_string(std::size_t count, char ch)
+	{
+		if (count > N)
+			throw std::out_of_range("basic_small_string::basic_small_string");
+
+		std::fill_n(std::begin(_data), count, ch);
+		_size = count;
+	}
+
+	// T can be another basic_small_string, std::string, std::vector, std::array, ...
+	template <typename T>
+	basic_small_string(const T& other, std::size_t pos)
+	{
+		init(other.data() + pos, other.size() - pos);
+	}
+
+	template <typename T>
+	basic_small_string(const T& other, std::size_t pos, std::size_t count)
+	{
+		init(other.data() + pos, count);
+	}
+
+	basic_small_string(const char* str, std::size_t count)
+	{
+		init(str, count);
+	}
+
+	basic_small_string(const char* str)
+	{
+		init(str, std::strlen(str));
+	}
+
+	template <typename InputIt>
+	basic_small_string(InputIt first, InputIt last)
+	{
+		init(first, last);
+	}
+
 	bool empty() const { return _size == 0; }
 	std::size_t size() const { return _size; }
 
 private:
+	void init(const char* str, std::size_t count)
+	{
+		if (count > N)
+			throw std::out_of_range("basic_small_string::init");
+
+		std::copy(str, str + count, std::begin(_data));
+		_size = count;
+	}
+
+	template <typename InputIt>
+	void init(InputIt first, InputIt last)
+	{
+		const std::size_t count = std::distance(first, last);
+		if (count > N)
+			throw std::out_of_range("basic_small_string::init");
+
+		std::copy(first, last, std::begin(_data));
+		_size = count;
+	}
+
 	std::array<char, N> _data;
 	char _size;
 };
